cb0a7badf3a2a7cefdf6db4d69e5c53e9d6a412d
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-07-15 13:34:27 +0000

    [update]レイアウト修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,12 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-// import "script.js"
+import "@fortawesome/fontawesome-free/js/all";
+import "script.js";
 import "jquery";
 import "popper.js";
 import "bootstrap";
---
+++ app/javascript/script.js
@@ -0,0 +1,5 @@
+$(function() {
+  // 一旦hide()で隠してフェードインさせる
+  $('div').hide().fadeIn('slow');
+ 
+});
