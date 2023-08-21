06df38d3e8dcb909e0a2478347ef7307bd543c16
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-21 10:30:37 +0000

    ロード画面

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,4 +1,22 @@
-$(document).on('turbolinks:load', function() {
-  // 一旦hide()で隠してフェードインさせる
-  $('div').hide().fadeIn('slow');
+// $(document).on('turbolinks:load', function() {
+//   // 一旦hide()で隠してフェードインさせる
+//   $('div').hide().fadeIn('slow');
+// });
+document.addEventListener('turbolinks:load', () => {
+  const loadingAreaGrey = document.querySelector('#loading');
+
+  window.addEventListener('load', () => {
+    loadingAreaGrey.animate(
+      {
+        opacity: [1, 0],
+        visibility: 'hidden',
+      },
+      {
+        duration: 2000,
+        delay: 1200,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+    );
+  });
 });
