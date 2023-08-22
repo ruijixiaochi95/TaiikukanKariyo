17b6533ef35933d9cb7e7429af09de96bb4be75a
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-22 10:09:14 +0000

    ロード画面

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@
 
 import "jquery";
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/js/all";
